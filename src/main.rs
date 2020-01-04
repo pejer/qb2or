59b7fff76e7dcc304d904fdb0afa65935263ba4e
Henrik Pejer <mrpejer@gmail.com>
2020-01-04 08:41:05 +0100

    Suggestions from cargo fmt/clippy.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate image;
 
-use  std::env;
 use image::GenericImageView;
+use std::env;
 
 /*
  * This will generate a string with which QMK and its oled_write_raw_P-method can use
  * to render a bitmap image on an oled.
  *
- * We assume that you'll create an image that has the correct dimensions and that is 
+ * We assume that you'll create an image that has the correct dimensions and that is
  * monochrome. We read the 'luma' value of each pixel and if its above 127, we consider
  * it to be 'on'.
  *
- * There are no checks of sane input so this little thing _could_ blow up in our 
+ * There are no checks of sane input so this little thing _could_ blow up in our
  * face.
  *
  * The oled_write_raw_P-method expects a string of numbers where it will use the
@@ -25,17 +25,17 @@ use image::GenericImageView;
  */
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() != 2 {
         println!("Usage {:?} <path to image file>", args[0]);
         return;
     }
     let img = image::open(&args[1]).unwrap();
-    let bytesize:u32 = 8;
+    let bytesize: u32 = 8;
     let mut counter: u32 = 0;
     let height_limit = img.height() - bytesize;
-    let width_limit  = img.width() - 1;
-    for y in 0..(img.height()/bytesize) {
+    let width_limit = img.width() - 1;
+    for y in 0..(img.height() / bytesize) {
         for x in 0..img.width() {
             let y_offset: u32 = y * bytesize;
             let mut byte: u8 = 0;
@@ -43,16 +43,16 @@ fn main() {
                 let realy = y_offset + i;
                 let pixel_value = image::Pixel::to_luma(&img.get_pixel(x, realy))[0];
                 if pixel_value > 127 {
-                    byte |= i32::pow(2,i) as u8;
+                    byte |= i32::pow(2, i) as u8;
                 }
             }
             print!("{}", byte);
             if !(y_offset == height_limit && x == width_limit) {
-               print!(","); 
+                print!(",");
             }
             counter += 1;
             if counter % img.width() == 0 {
-                println!("");
+                println!();
             }
         }
     }
